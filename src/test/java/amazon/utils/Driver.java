72123c7eed1a9318b8c569df5315fd0a56716c48
Esma Yılmaz <elektroyazilim@gmail.com>
2023-03-30 12:39:43 +0300

    deleted unnecssary libraries

--- src/test/java/amazon/utils/Driver.java
+++ src/test/java/amazon/utils/Driver.java
@@ -6,10 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import java.io.IOException;
+
 import java.time.Duration;
 
 public class Driver {
