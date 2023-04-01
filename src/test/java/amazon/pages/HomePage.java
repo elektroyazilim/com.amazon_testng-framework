2624f84cf693a5166793773543c14baa56fb06d5
Esma Yılmaz <elektroyazilim@gmail.com>
2023-04-01 17:16:30 +0300

    US001 TC004 is fixed..

--- src/test/java/amazon/pages/HomePage.java
+++ src/test/java/amazon/pages/HomePage.java
@@ -6,12 +6,17 @@ import org.openqa.selenium.support.FindBy;
 import java.util.List;
 
 public class HomePage extends Base {
+    @FindBy(css = "div#navbar-backup-backup")
+    public List<WebElement> fakeBar;
 
     @FindBy(id = "twotabsearchtextbox")
     public WebElement searchBox;
 
     @FindBy(xpath = "//ul[@role='list']/li")
     public List<WebElement> elements;
+
+    @FindBy(xpath = "//ul[@role='list']/li[1]//a")
+    public WebElement firstProductInHome;
 
 
 
