eccd11e609c9c1e3f2e54b9551203c1e70b5617f
Esma Yılmaz <elektroyazilim@gmail.com>
2023-04-12 00:56:00 +0300

    log4j integration is completed

--- src/test/java/amazon/tests/US004_PaginationTest.java
+++ src/test/java/amazon/tests/US004_PaginationTest.java
@@ -7,7 +7,11 @@ import amazon.utils.BrowserUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import org.apache.logging.log4j.*;
+
 public class US004_PaginationTest {
+
+    private static final Logger log = LogManager.getLogger(US004_PaginationTest.class);
     HomePage homePage = new HomePage();
     SearchPage searchPage = new SearchPage();
 
@@ -15,39 +19,47 @@ public class US004_PaginationTest {
     public void TC001_PreviousDisable() // first page
     {
         String searchText = "headphone";
-        AmazonUtils.takeMeToSearchPage(homePage,searchText);
+        AmazonUtils.takeMeToSearchPage(homePage, searchText);
+        log.info("We are at search page for " + searchText);
 
         // search page
         BrowserUtils.scrollToElement(searchPage.previousBtn);
+        log.debug("Scroll to previous button..");
 
         //String tagName = searchPage.previousBtn.getTagName(); // a ise enable, span disable
         String className = searchPage.previousBtn.getAttribute("class"); // s-pagination-item s-pagination-previous s-pagination-disabled
 
-        if(AmazonUtils.IsPaginationEnabled(className)) // previous btn disable
+        if (AmazonUtils.IsPaginationEnabled(className)) // previous btn disable
         {
+            log.error("Previous button must be disable");
             Assert.assertTrue(false);
+        } else {
+            log.info("Test passed successfully.");
         }
     }
+
     @Test
-    public void TC002_NextEnable()
-    {
+    public void TC002_NextEnable() {
         String searchText = "dress";
-        AmazonUtils.takeMeToSearchPage(homePage,searchText);
+        AmazonUtils.takeMeToSearchPage(homePage, searchText);
+        log.info("In search page for " + searchText);
 
         BrowserUtils.scrollToElement(searchPage.nextBtn);
+        log.debug("Scrolling is successfully..");
 
         String className = searchPage.nextBtn.getAttribute("class");
         // String tagName = searchPage.nextBtn.getTagName();
 
-        if (AmazonUtils.IsPaginationEnabled(className) == false)
-        {
+        if (AmazonUtils.IsPaginationEnabled(className) == false) {
+            log.error("Test failed...");
             Assert.assertTrue(false);
+
         }
+        log.info("Test passed succesfully..");
     }
 
     @Test
-    public void TC003_PrevNextCheck()
-    {
+    public void TC003_PrevNextCheck() {
         String anyText = "dress";
         AmazonUtils.takeMeToSearchPage(homePage, anyText);
 
@@ -63,5 +75,6 @@ public class US004_PaginationTest {
 
         String nextClass = searchPage.nextBtn.getAttribute("class");
         Assert.assertTrue(AmazonUtils.IsPaginationEnabled(nextClass));
+        log.info("Test passed succesfully..");
     }
 }
